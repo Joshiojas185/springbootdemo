628a93a33d860d23b048b66c427ef52afb7d87c8
Ojas Joshi <joshiojas185@gmail.com>
2026-03-15 22:29:18 +0530

    added addstudent

--- src/main/java/com/springbootdemo/springbootdemo/StudentService.java
+++ src/main/java/com/springbootdemo/springbootdemo/StudentService.java
@@ -1,18 +1,24 @@
 package com.springbootdemo.springbootdemo;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService {
     private final List<Student> students = new ArrayList<>();
+    private final ModelMapper modelMapper;
 
     public List<Student> getAllStudents() {
         return students;
     }
 
-    public Student addStudent(Student student) {
+    public Student addStudent(AddStudent addStudent) {
+
+        Student student = modelMapper.map(addStudent,Student.class);
         students.add(student);
         return student;
     }
